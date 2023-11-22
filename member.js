bec9c99bf1c306421125aeb51ef7bdf61d14d6ba
Victor David Cerrud <victor.cerrud3686@itse.ac.pa>
2023-11-22 05:15:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){  
+    var skills = ["HTML", "CSS", "JS", "React", "Node"];
+    var member = {
+        name: "John",
+        age: 22,
+        skills: skills
+    }
+    return member;
+}
